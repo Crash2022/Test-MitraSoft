83bf7174498e5b5741a779325fb9f43139bdcf3c
Crash2022 <crash555@mail.ru>
2023-05-24 02:09:39 +0500

    add: Provider, react-redux, try fetch posts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
 import './App.css';
+import React, { useEffect } from 'react';
+import { useAppDispatch } from '../shared/hooks/useAppDispatch';
+import { useAppSelector } from '../shared/hooks/useAppSelector';
+import { getPostsTC } from '../store/posts-reducer';
+import { selectPosts } from '../store/selectors';
+import { PostType } from '../shared/types/types';
 
 export const App = () => {
+
+    const dispatch = useAppDispatch()
+    const posts = useAppSelector(selectPosts)
+
+    useEffect(() => {
+        dispatch(getPostsTC())
+    }, [])
+
     return (
         <div className="App">
-           MainPage
+            {
+                posts.map((p: PostType) => {
+                    return (
+                        <div key={p.id}>{p.title}</div>
+                    )
+                })
+            }
         </div>
     )
 }
--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -4,6 +4,8 @@ import { select } from 'redux-saga/effects';
 export const selectAppStatus = (state: AppRootStateType) => state.app.status
 export const selectAppError = (state: AppRootStateType) => state.app.error
 
+export const selectPosts = (state: AppRootStateType) => state.posts
+
 // selector for saga
 // export function* appSelect<TSelected>(selector: (state: AppRootStateType) => TSelected, ): Generator<any, TSelected, TSelected> {
 //     return yield select(selector);
