ad94a83535da40bb063b106838db4c5dbf0fba08
Crash2022 <crash555@mail.ru>
2023-05-24 16:53:26 +0500

    add: back button

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,13 +1,17 @@
 import React, {useEffect} from 'react';
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {useAppDispatch} from "../../shared/hooks/useAppDispatch";
 import {getUserTC} from "../../store/user-reducer";
 import {useAppSelector} from "../../shared/hooks/useAppSelector";
 import {selectUser} from "../../store/selectors";
+// import Breadcrumb from "react-bootstrap/Breadcrumb";
+import {RoutePaths} from "../../shared/api/paths";
+import {Button} from "react-bootstrap";
 
 export const User = () => {
 
     const dispatch = useAppDispatch()
+    const navigate = useNavigate()
     const params = useParams<'userId'>()
     const user = useAppSelector(selectUser)
 
@@ -17,6 +21,15 @@ export const User = () => {
 
     return (
         <div>
+            {/*<div>*/}
+            {/*    <Breadcrumb>*/}
+            {/*        <Breadcrumb.Item href={RoutePaths.HOME}>Посты</Breadcrumb.Item>*/}
+            {/*        <Breadcrumb.Item active>Пользователь: ID={params.userId}</Breadcrumb.Item>*/}
+            {/*    </Breadcrumb>*/}
+            {/*</div>*/}
+
+            <Button variant="primary" onClick={() => {navigate(RoutePaths.HOME)}}>Вернуться к постам</Button>
+
             {
                 user && (
                     <>
