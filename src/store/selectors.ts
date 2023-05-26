d64d6a9ac3d741bcef1bb1978068e33ddf3f62a2
Crash2022 <crash555@mail.ru>
2023-05-26 21:05:25 +0500

    refactor: code

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,16 +1,8 @@
 import {AppRootStateType} from './store';
-import { select } from 'redux-saga/effects';
 
 export const selectAppStatus = (state: AppRootStateType) => state.app.status
 export const selectAppLocalStatus = (state: AppRootStateType) => state.app.localStatus
-export const selectAppError = (state: AppRootStateType) => state.app.error
 
 export const selectPosts = (state: AppRootStateType) => state.posts.allPosts
 export const selectUser = (state: AppRootStateType) => state.posts.userProfile
 export const selectUserPosts = (state: AppRootStateType) => state.posts.userPosts
-// export const selectPostComments = (state: AppRootStateType) => state.posts.postComments
-
-// selector for saga
-// export function* appSelect<TSelected>(selector: (state: AppRootStateType) => TSelected, ): Generator<any, TSelected, TSelected> {
-//     return yield select(selector);
-// }
